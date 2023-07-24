db597f3f7b3d4aac200b41b47a79a8e2b0c8f7ba
Ziko <Ziko@gmail.com>
2023-07-24 15:51:38 +0100

    benifits tts section

---
+++ storage/framework/views/e42759cae0799db1db9bae88b1e1559d3040354b.php
@@ -0,0 +1,31 @@
+<!--Favicon -->
+<link rel="icon" href="<?php echo e(URL::asset('img/brand/favicon.ico')); ?>" type="image/x-icon"/>
+
+<!-- Animate css -->
+<link href="<?php echo e(URL::asset('css/animated.css')); ?>" rel="stylesheet" />
+
+<!--Sidemenu css -->
+<link href="<?php echo e(URL::asset('css/sidemenu.css')); ?>" rel="stylesheet">
+
+<!-- P-scroll bar css-->
+<link href="<?php echo e(URL::asset('plugins/p-scrollbar/p-scrollbar.css')); ?>" rel="stylesheet" />
+
+<!---Icons css-->
+<link href="<?php echo e(URL::asset('css/icons.css')); ?>" rel="stylesheet" />
+
+<!-- Style css -->
+<link href="<?php echo e(URL::asset('plugins/tippy/scale-extreme.css')); ?>" rel="stylesheet" />
+<link href="<?php echo e(URL::asset('plugins/tippy/material.css')); ?>" rel="stylesheet" />
+
+<?php echo $__env->yieldContent('css'); ?>
+
+<!-- Style css -->
+<link href="<?php echo e(URL::asset('css/app.css')); ?>" rel="stylesheet" />
+
+<!-- Simplebar css -->
+<link href="<?php echo e(URL::asset('plugins/simplebar/css/simplebar.css')); ?>" rel="stylesheet">
+
+<!-- Color Skin CSS -->
+<link href="<?php echo e(URL::asset('css/color.css')); ?>" rel="stylesheet" />
+
+	<?php /**PATH C:\xampp\htdocs\texttospeechapp\resources\views/layouts/header.blade.php ENDPATH**/ ?>
